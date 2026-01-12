48590117b0f56f203397a58bf2b2beeaa7939985
Drexel2k <13720303+Drexel2k@users.noreply.github.com>
2026-01-12 23:57:04 +0100

    Migrated settings from PageView to tabs.

--- src/lib/ui/screens/settings_screen.dart
+++ src/lib/ui/screens/settings_screen.dart
@@ -1,10 +1,8 @@
 import "package:flutter/material.dart";
 import "package:openeatsjournal/l10n/app_localizations.dart";
-import "package:openeatsjournal/ui/screens/settings_screen_viewmodel.dart";
-import "package:openeatsjournal/domain/utils/open_eats_journal_strings.dart";
-import "package:openeatsjournal/ui/utils/setting_type.dart";
-import "package:openeatsjournal/ui/screens/settings_screen_page_personal.dart";
 import "package:openeatsjournal/ui/screens/settings_screen_page_app.dart";
+import "package:openeatsjournal/ui/screens/settings_screen_page_personal.dart";
+import "package:openeatsjournal/ui/screens/settings_screen_viewmodel.dart";
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key, required SettingsScreenViewModel settingsScreenViewModel}) : _settingsScreenViewModel = settingsScreenViewModel;
@@ -18,8 +16,6 @@ class SettingsScreen extends StatefulWidget {
 class _SettingsScreenState extends State<SettingsScreen> {
   late SettingsScreenViewModel _settingsScreenViewModel;
 
-  final _pageViewController = PageController();
-
   //only called once even if the widget is recreated on opening the virtual keyboard e.g.
   @override
   void initState() {
@@ -29,63 +25,29 @@ class _SettingsScreenState extends State<SettingsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        ValueListenableBuilder(
-          valueListenable: _settingsScreenViewModel.currentPageIndex,
-          builder: (_, _, _) {
-            String pageTitle = OpenEatsJournalStrings.emptyString;
-            if (_settingsScreenViewModel.currentPageIndex.value == 0) {
-              pageTitle = AppLocalizations.of(context)!.personal_settings;
-            } else if (_settingsScreenViewModel.currentPageIndex.value == 1) {
-              pageTitle = AppLocalizations.of(context)!.app_settings;
-            }
-
-            return AppBar(backgroundColor: Color.fromARGB(0, 0, 0, 0), title: Text(pageTitle));
-          },
-        ),
-        ValueListenableBuilder(
-          valueListenable: _settingsScreenViewModel.currentPageIndex,
-          builder: (_, _, _) {
-            return SegmentedButton<SettingType>(
-              selected: <SettingType>{SettingType.getByValue(_settingsScreenViewModel.currentPageIndex.value + 1)},
-              showSelectedIcon: false,
-              segments: [
-                ButtonSegment<SettingType>(value: SettingType.personal, label: Text(AppLocalizations.of(context)!.personal_settings_abbreviated)),
-                ButtonSegment<SettingType>(value: SettingType.app, label: Text(AppLocalizations.of(context)!.app_settings)),
-              ],
-              onSelectionChanged: (Set<SettingType> newSelection) {
-                if (newSelection.single == SettingType.app) {
-                  if (_settingsScreenViewModel.currentPageIndex.value == 0) {
-                    _movePageIndex(1);
-                  }
-                } else {
-                  if (_settingsScreenViewModel.currentPageIndex.value == 1) {
-                    _movePageIndex(0);
-                  }
-                }
-              },
-            );
-          },
-        ),
-        SizedBox(height: 10),
-        Expanded(
-          child: PageView(
-            controller: _pageViewController,
-            physics: NeverScrollableScrollPhysics(),
-            children: <Widget>[
-              SettingsScreenPagePersonal(settingsScreenViewModel: _settingsScreenViewModel),
-              SettingsScreenPageApp(settingsViewModel: _settingsScreenViewModel),
+    return DefaultTabController(
+      length: 2,
+      child: Column(
+        children: [
+          AppBar(backgroundColor: Color.fromARGB(0, 0, 0, 0), title: Text(AppLocalizations.of(context)!.settings)),
+          TabBar(
+            tabs: [
+              Tab(child: Text(AppLocalizations.of(context)!.personal)),
+              Tab(child: Text(AppLocalizations.of(context)!.app)),
             ],
           ),
-        ),
-      ],
+          SizedBox(height: 10),
+          Expanded(
+            child: TabBarView(
+              children: [
+                SettingsScreenPagePersonal(settingsScreenViewModel: _settingsScreenViewModel),
+                SettingsScreenPageApp(settingsViewModel: _settingsScreenViewModel),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
-  }
-
-  void _movePageIndex(int page) {
-    _settingsScreenViewModel.currentPageIndex.value = page;
-    _pageViewController.animateToPage(_settingsScreenViewModel.currentPageIndex.value, duration: const Duration(milliseconds: 400), curve: Curves.easeInOut);
   }
 
   @override
@@ -94,7 +56,6 @@ class _SettingsScreenState extends State<SettingsScreen> {
     if (widget._settingsScreenViewModel != _settingsScreenViewModel) {
       _settingsScreenViewModel.dispose();
     }
-    _pageViewController.dispose();
 
     super.dispose();
   }
