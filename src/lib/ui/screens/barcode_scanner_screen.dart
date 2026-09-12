33aa6fab2b01be62354f30d561e4257672315721
Drexel2k <13720303+Drexel2k@users.noreply.github.com>
2026-09-12 14:33:35 +0200

    Updated barcode scanner to use zxing internal gallery.

--- src/lib/ui/screens/barcode_scanner_screen.dart
+++ src/lib/ui/screens/barcode_scanner_screen.dart
@@ -1,6 +1,5 @@
 import "package:flutter/material.dart";
 import "package:flutter_zxing/flutter_zxing.dart";
-import "package:image_picker/image_picker.dart";
 import "package:openeatsjournal/app_global.dart";
 import "package:openeatsjournal/l10n/app_localizations.dart";
 import "package:openeatsjournal/ui/utils/overlay_display.dart";
@@ -21,56 +20,29 @@ class _BarcodeScannerScreenState extends State<BarcodeScannerScreen> {
   Widget build(BuildContext context) {
     final ColorScheme colorScheme = Theme.of(context).colorScheme;
 
-    return Stack(
-      children: [
-        ReaderWidget(
-          showToggleCamera: false,
-          showGallery: false,
-          cropPercent: 0.9,
-          actionButtonsBackgroundColor: colorScheme.surface,
-          flashOnIcon: Icon(Icons.flash_on, color: colorScheme.primary),
-          flashOffIcon: Icon(Icons.flash_off, color: colorScheme.primary),
+    return ReaderWidget(
+      showToggleCamera: false,
+      cropPercent: 0.9,
+      actionButtonsBackgroundColor: colorScheme.surface,
+      flashOnIcon: Icon(Icons.flash_on, color: colorScheme.primary),
+      flashOffIcon: Icon(Icons.flash_off, color: colorScheme.primary),
 
-          onScan: (result) {
-            if (result.isValid && !_barcodeReturned) {
-              _barcodeReturned = true;
-              Navigator.pop(context, result.text);
-            }
-          },
-        ),
+      onScan: (result) {
+        if (result.isValid && !_barcodeReturned) {
+          _barcodeReturned = true;
+          Navigator.pop(context, result.text);
+        }
+      },
 
-        Positioned(
-          bottom: 34,
-          right: 9,
-          child: Material(
-            color: colorScheme.surface,
-            borderRadius: BorderRadius.circular(10),
-            child: InkWell(
-              borderRadius: BorderRadius.circular(10),
-              onTap: () async {
-                final XFile? file = await ImagePicker().pickImage(source: ImageSource.gallery);
+      onScanFailure: (result) {
+        if (result.source != null && result.source == CodeSource.localImageFile) {
+          final OverlayDisplay overlayDisplay = Provider.of<OverlayDisplay>(AppGlobal.navigatorKey.currentContext!, listen: false);
 
-                if (file != null) {
-                  final Code result = await zx.readBarcodeImagePath(file, DecodeParams(format: Format.any, tryHarder: true, tryInverted: true));
-
-                  if (result.isValid && !_barcodeReturned) {
-                    _barcodeReturned = true;
-                    Navigator.pop(AppGlobal.navigatorKey.currentContext!, result.text);
-                    return;
-                  }
-
-                  final OverlayDisplay overlayDisplay = Provider.of<OverlayDisplay>(AppGlobal.navigatorKey.currentContext!, listen: false);
-
-                  overlayDisplay.enqueue(
-                    overlayInfo: OverlayInfo(message: AppLocalizations.of(AppGlobal.navigatorKey.currentContext!)!.no_barcode_found_on_picture, spacer: 170),
-                  );
-                }
-              },
-              child: SizedBox(width: 48, height: 48, child: Icon(Icons.photo_library, color: colorScheme.primary, size: 26)),
-            ),
-          ),
-        ),
-      ],
+          overlayDisplay.enqueue(
+            overlayInfo: OverlayInfo(message: AppLocalizations.of(AppGlobal.navigatorKey.currentContext!)!.no_barcode_found_on_picture, spacer: 170),
+          );
+        }
+      },
     );
   }
 }
